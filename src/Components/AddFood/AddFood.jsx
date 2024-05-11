009a9424a5708bdc6fb4e23ea131fee9d8fbeebf
juwel-hub <jr3764920@gmail.com>
2024-05-12 00:10:40 +0600

    add food input fild added

--- src/Components/AddFood/AddFood.jsx
+++ src/Components/AddFood/AddFood.jsx
@@ -1,9 +1,214 @@
-const AddFood = () => {
+// import React, { useState } from "react";
+
+function AddFood() {
+  // const [formData, setFormData] = useState({
+  //   foodName: "",
+  //   foodImage: "",
+  //   foodQuantity: "",
+  //   pickupLocation: "",
+  //   expireDateTime: "",
+  //   additionalNotes: "",
+  //   donatorImage: "",
+  //   donatorName: "John Doe",
+  //   donatorEmail: "",
+  //   foodStatus: "available",
+  // });
+
+  // const handleChange = (e) => {
+  //   setFormData({
+  //     ...formData,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const foodName = form.foodName.value;
+    const foodImage = form.foodImage.value;
+    const foodQuantity = form.foodQuantity.value;
+    const pickupLocation = form.pickupLocation.value;
+    const expireDateTime = form.expireDateTime.value;
+    const additionalNotes = form.additionalNotes.value;
+    const donatorImage = form.donatorImage.value;
+    const donatorName = form.foodName.value;
+    const donatorEmail = form.donatorEmail.value;
+    const informations = {
+      foodName,
+      foodImage,
+      foodQuantity,
+      pickupLocation,
+      expireDateTime,
+      additionalNotes,
+      donatorImage,
+      donatorEmail,
+      donatorName,
+    };
+    console.log(informations);
+  };
+
   return (
-    <div>
-      <h1>Add food</h1>
+    <div className=" min-h-screen w-full mx-auto  mt-8 p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">Add Food</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="md:flex items-center justify-center gap-5">
+          <div className="w-[50%]">
+            <div className="mb-4">
+              <label
+                htmlFor="foodName"
+                className="block font-medium text-gray-700"
+              >
+                Food Name:
+              </label>
+              <input
+                type="text"
+                id="foodName"
+                name="foodName"
+                placeholder="Food Name"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="foodImage"
+                className="block font-medium text-gray-700"
+              >
+                Food Image URL:
+              </label>
+              <input
+                type="text"
+                id="foodImage"
+                placeholder="Food Image"
+                name="foodImage"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="foodQuantity"
+                className="block font-medium text-gray-700"
+              >
+                Food Quantity:
+              </label>
+              <input
+                type="text"
+                id="foodQuantity"
+                name="foodQuantity"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="pickupLocation"
+                className="block font-medium text-gray-700"
+              >
+                Pickup Location:
+              </label>
+              <input
+                type="text"
+                id="pickupLocation"
+                name="pickupLocation"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="expireDateTime"
+                className="block font-medium text-gray-700"
+              >
+                Expired Date/Time:
+              </label>
+              <input
+                type="datetime-local"
+                id="expireDateTime"
+                name="expireDateTime"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+          </div>
+          <div className="w-[50%]">
+            {" "}
+            <div className="mb-4">
+              <label
+                htmlFor="additionalNotes"
+                className="block font-medium text-gray-700"
+              >
+                Additional Notes:
+              </label>
+              <textarea
+                id="additionalNotes"
+                name="additionalNotes"
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              ></textarea>
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="donatorImage"
+                className="block font-medium text-gray-700"
+              >
+                Donator Image URL:
+              </label>
+              <input
+                type="text"
+                id="donatorImage"
+                name="donatorImage"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="donatorName"
+                className="block font-medium text-gray-700"
+              >
+                Donator Name:
+              </label>
+              <input
+                type="text"
+                id="donatorName"
+                name="donatorName"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="donatorEmail"
+                className="block font-medium text-gray-700"
+              >
+                Donator Email:
+              </label>
+              <input
+                type="email"
+                id="donatorEmail"
+                name="donatorEmail"
+                required
+                className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+          </div>
+        </div>
+
+        <input type="" id="foodStatus" name="foodStatus" value="available" />
+
+        <button
+          type="submit"
+          className="w-full  text-white font-bold btn rounded bg-orange-400 focus:outline-none"
+        >
+          Add Food
+        </button>
+      </form>
     </div>
   );
-};
+}
 
 export default AddFood;
